c347d9b36dc1b3a6ffd95a7de20fa6660e73c93c
brandonngiam <brandonngiam@user.noreply.github.com>
2019-07-10 16:44:59 +0800

    Setup

--- account.js
+++ account.js
@@ -1,1 +1,3 @@
 class Account {}
+
+module.exports = Account;
---
+++ account.test.js
@@ -0,0 +1,7 @@
+const Account = require("./account");
+
+describe("Bank account", () => {
+  it("basic test", () => {
+    expect(1).toEqual(1);
+  });
+});
